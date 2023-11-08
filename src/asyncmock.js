af0940f0256cb4752f92beb9ff69c1b43c75b84b
Luciano Miranda <lu.mer.zoni@gmail.com>
2023-11-08 01:22:42 -0300

    contador sincronizado

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,14 +1,14 @@
 const productosBerlini = [
-    { id: "1", nombre: "Sorrentinos", precio: 2000, img: "../img/sorrentinos.jpg", idCat:"1" },
-    { id: "2", nombre: "Sorrentinos", precio: 2000, img: "../img/sorrentinos.jpg", idCat:"1" },
-    { id: "3", nombre: "Sorrentinos", precio: 2000, img: "../img/sorrentinos.jpg", idCat:"1" },
-    { id: "4", nombre: "Tallarines", precio: 1800, img: "../img/tallarines.jpg", idCat:"2" },
-    { id: "5", nombre: "Tallarines", precio: 1800, img: "../img/tallarines.jpg", idCat:"2" },
-    { id: "6", nombre: "Tallarines", precio: 1800, img: "../img/tallarines.jpg", idCat:"2" },
-    { id: "7", nombre: "Tallarines", precio: 1800, img: "../img/tallarines.jpg", idCat:"2" },
-    { id: "8", nombre: "Salsas", precio: 1000, img: "../img/salsa.jpg", idCat:"3" },
-    { id: "9", nombre: "Salsas", precio: 1000, img: "../img/salsa.jpg", idCat:"3" },
-    { id: "10", nombre: "Salsas", precio: 1000, img: "../img/salsa.jpg", idCat:"3" },
+    { id: "1", nombre: "Sorrentinos", stock: 10, precio: 2000, img: "../img/sorrentinos.jpg", idCat:"1" },
+    { id: "2", nombre: "Sorrentinos", stock: 10, precio: 2000, img: "../img/sorrentinos.jpg", idCat:"1" },
+    { id: "3", nombre: "Sorrentinos", stock: 10, precio: 2000, img: "../img/sorrentinos.jpg", idCat:"1" },
+    { id: "4", nombre: "Tallarines", stock: 10, precio: 1800, img: "../img/tallarines.jpg", idCat:"2" },
+    { id: "5", nombre: "Tallarines", stock: 10, precio: 1800, img: "../img/tallarines.jpg", idCat:"2" },
+    { id: "6", nombre: "Tallarines", stock: 10, precio: 1800, img: "../img/tallarines.jpg", idCat:"2" },
+    { id: "7", nombre: "Tallarines", stock: 10, precio: 1800, img: "../img/tallarines.jpg", idCat:"2" },
+    { id: "8", nombre: "Salsas", stock: 10, precio: 1000, img: "../img/salsa.jpg", idCat:"3" },
+    { id: "9", nombre: "Salsas", stock: 10, precio: 1000, img: "../img/salsa.jpg", idCat:"3" },
+    { id: "10", nombre: "Salsas", stock: 10, precio: 1000, img: "../img/salsa.jpg", idCat:"3" },
 ]
 
 //funcion para obtener todos los productos
--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,17 @@
+import { useState } from 'react';
+import  Contador  from '../Contador/Contador';
+import { Link } from 'react-router-dom';
 import './ItemDetail.css';
 
-const ItemDetail = ({ id, nombre, precio, img }) => {
+const ItemDetail = ({ id, nombre, precio, img, stock }) => {
+
+    const [agregarCantidad, setAgregarCantidad] = useState(0);
+
+    const manejadorCantidad = (cantidad) =>{
+        setAgregarCantidad(cantidad);
+        console.log("Productos agregados:" + cantidad);
+    }
+
     return (
         <div className='contenedor-detalles'>
 
@@ -9,11 +20,19 @@ const ItemDetail = ({ id, nombre, precio, img }) => {
             </div>
 
             <div className='contenedor-descripcion'>
+                <div className='titulo-descripcion'>
                 <h2>{nombre}</h2>
                 <h3>Precio: $ {precio}</h3>
+                <h3>Id: {id}</h3>
+                </div>
+                
                 <p>Descripcion: Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo magnam distinctio officia nobis, pariatur ipsam fugit corrupti deleniti sunt dolorem est voluptatem commodi hic non. Deleniti quae repellat architecto eveniet.
                 Quasi vitae corrupti magni unde nisi maiores ipsa enim repudiandae sint debitis ratione repellat expedita, autem optio ipsam incidunt nesciunt aliquid. Corporis facilis sunt perspiciatis eos quod quibusdam blanditiis vel!
                 </p>
+
+                {
+                      agregarCantidad > 0 ? (<Link to="/cart">Terminar Compra</Link>) : (<Contador inicial = {1} stock={stock} funcionAgregar={manejadorCantidad}/>)
+                }
             </div>
         </div>
     )
